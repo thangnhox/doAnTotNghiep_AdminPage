66c3fd9a0ed2f912789964b9bb8908bfa542f75b
kuro2904 <trung2001sgp@gmail.com>
2024-11-08 09:18:55 +0700

    Refactor and update MainRouter

--- thu-vien-sach/src/routers/MainRouter.tsx
+++ thu-vien-sach/src/routers/MainRouter.tsx
@@ -1,62 +1,42 @@
-import { Avatar, Button, Layout } from "antd";
-import { Content, Header } from "antd/es/layout/layout";
+import { Layout } from "antd";
+import { Content } from "antd/es/layout/layout";
 
-import { useState } from "react";
-import { AiOutlineMenuFold, AiOutlineMenuUnfold } from "react-icons/ai";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HeaderComponent from "../pages/components/HeaderComponent";
 import SiderComponent from "../pages/components/SiderComponent";
+import AuthorPage from "../pages/main/authors/AuthorPage";
+import AddBookPage from "../pages/main/book/AddBookPage";
 import BookPage from "../pages/main/book/BookPage";
+import CategoryDetail from "../pages/main/category/CategoryDetail";
 import CategoryPage from "../pages/main/category/CategoryPage";
 import DiscountPage from "../pages/main/discount/DiscountPage";
 import HomePage from "../pages/main/home/HomePage";
+import MembershipPage from "../pages/main/membership/MembershipPage";
 import OrderPage from "../pages/main/order/OrderPage";
+import PublisherDetail from "../pages/main/publisher/PublisherDetail";
+import PublisherPage from "../pages/main/publisher/PublisherPage";
 
 const MainRouter = () => {
-  const [isMenuCollapsed, setIsMenuCollapsed] = useState(false);
-
   return (
-    <div className="container-fluid ">
+    <div className="container-fluid p-0 m-0">
       <BrowserRouter>
         <Layout>
-          <SiderComponent collapsed={isMenuCollapsed} />
+          <SiderComponent />
           <Layout>
-            <Header
-              style={{ padding: 0, backgroundColor: "red" }}
-              className="row"
-            >
-              <Button
-                type="text"
-                icon={
-                  isMenuCollapsed ? (
-                    <AiOutlineMenuUnfold />
-                  ) : (
-                    <AiOutlineMenuFold />
-                  )
-                }
-                onClick={() => setIsMenuCollapsed}
-                style={{
-                  fontSize: "16px",
-                  width: 64,
-                  height: 64,
-                  backgroundColor: "white",
-                }}
-              />
-              <div className="row">
-                <Avatar
-                  size={50}
-                  alt="User Avatar image"
-                  src={""}
-                  style={{ marginRight: "8px" }}
-                />
-              </div>
-            </Header>
+            <HeaderComponent />
             <Content className="m-3 p-3">
               <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/book" element={<BookPage />} />
-                <Route path="/category" element={<CategoryPage />} />
-                <Route path="/discount" element={<DiscountPage />} />
-                <Route path="/order" element={<OrderPage />} />
+                <Route path="/books" element={<BookPage />} />
+                <Route path="/books/add-book" element={<AddBookPage />} />
+                <Route path="/authors" element={<AuthorPage />} />
+                <Route path="/publishers" element={<PublisherPage />} />
+                <Route path="/publishers/:id" element={<PublisherDetail />} />
+                <Route path="/categories" element={<CategoryPage />} />
+                <Route path="/categories/:id" element={<CategoryDetail />} />
+                <Route path="/memberships" element={<MembershipPage />} />
+                <Route path="/discounts" element={<DiscountPage />} />
+                <Route path="/orders" element={<OrderPage />} />
               </Routes>
             </Content>
           </Layout>
