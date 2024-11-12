f1dc598b37e3e6842bf38bb0e636b055631ea2dc
kuro2904 <trung2001sgp@gmail.com>
2024-11-12 20:18:31 +0700

    Add  Add Book Page + upload pdf file

--- thu-vien-sach/src/pages/main/book/AddBookPage.tsx
+++ thu-vien-sach/src/pages/main/book/AddBookPage.tsx
@@ -1,13 +1,44 @@
-import { InboxOutlined } from "@ant-design/icons";
-import { Button, Card, Form, Input, InputNumber, Upload } from "antd";
+import { Button, Card, Form, Input, InputNumber, message, Select } from "antd";
 import { useForm } from "antd/es/form/Form";
-import { getDownloadURL, uploadBytesResumable } from "firebase/storage";
-import { useState } from "react";
-import { pdfRef } from "../../../firebase/firebaseConfig";
+import { AxiosResponse } from "axios";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { useEffect, useState } from "react";
+import ResponseListDTO from "../../../dtos/Response/ResponseListDTO";
+import { fireStorage } from "../../../firebase/firebaseConfig";
+import Author from "../../../models/Author";
+import Category from "../../../models/Category";
+import Publisher from "../../../models/Publisher";
+import { handleAPI } from "../../../remotes/apiHandle";
+
+interface PageState {
+  isLoading: boolean;
+  authors: Author[];
+  categories: Category[];
+  publishers: Publisher[];
+}
+
+type SearchParams = {
+  value: number;
+  lable: string;
+};
 
 const AddBookPage = () => {
-  const [fileUpload, setFileUpload] = useState<any>();
+  const [state, setState] = useState<PageState>({
+    authors: [],
+    categories: [],
+    isLoading: false,
+    publishers: [],
+  });
+  const [fileUpload, setFileUpload] = useState<File | undefined>(undefined);
+
+  const [publisherParams, setPublisherParams] = useState<string | undefined>();
+
+  const [authorSelected, setAuthorSelected] = useState<
+    SearchParams | undefined
+  >(undefined);
+
   const [addBookForm] = useForm();
+
   const formItemLayout = {
     labelCol: {
       xs: { span: 24 },
@@ -18,42 +49,80 @@ const AddBookPage = () => {
       sm: { span: 14 },
     },
   };
-  const { Dragger } = Upload;
 
-  const handleAddBook = () => {
-    // TODO: Làm xong cái thể loại + tác giả thì quay lại làm tiếp
-    console.log(fileUpload);
-  };
-  const uploadPDF = async (file: File) => {
-    const uploadTask = uploadBytesResumable(pdfRef, file);
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        // Observe state change events such as progress, pause, and resume
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log("Upload is " + progress + "% done");
-        switch (snapshot.state) {
-          case "paused":
-            console.log("Upload is paused");
-            break;
-          case "running":
-            console.log("Upload is running");
-            break;
-        }
-      },
-      (error) => {
-        // Handle unsuccessful uploads
-      },
-      () => {
-        // Handle successful uploads on complete
-        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          console.log("File available at", downloadURL);
-        });
-      }
+  useEffect(() => {
+    try {
+      setState((prev) => ({
+        ...prev,
+        isLoading: true,
+      }));
+      getAuthors();
+      getCategories();
+      getPublisher(publisherParams);
+    } catch (error: any) {
+      message.error(error.message);
+    } finally {
+      setState((prev) => ({
+        ...prev,
+        isLoading: false,
+      }));
+    }
+  }, [publisherParams]);
+
+  const getAuthors = async () => {};
+
+  const getCategories = async () => {};
+
+  const getPublisher = async (publisherParams?: string) => {
+    const res: AxiosResponse<ResponseListDTO<Publisher[]>> = await handleAPI(
+      publisherParams ? `publisher/find/${publisherParams}` : `publisher`
     );
+    if (res.status === 200) {
+      setState((prev) => ({
+        ...prev,
+        publishers: res.data.data,
+      }));
+    }
+  };
+
+  const onChangePublisherSelect = (value: string) => {
+    // console.log(value);
+    // setAuthorSelected({
+    //   lable: value.lable,
+    //   value: value.value,
+    // });
+  };
+
+  const onSearchPublisherSelect = (value: string) => {
+    // console.log(value);
+  };
+
+  const handleAddBook = async () => {
+    setState((prev) => ({
+      ...prev,
+      isLoading: true,
+    }));
+    try {
+      if (!fileUpload) {
+        message.error("Chưa có file");
+        return;
+      }
+      const pdfRef = ref(fireStorage, `pdfs/${fileUpload.name}`);
+
+      const uploadFileReponse = await uploadBytes(pdfRef, fileUpload);
+      const urlLink: string = await getDownloadURL(uploadFileReponse.ref);
+
+      //Todo: handle request Back-end
+
+      message.success("Tải lên file thành công");
+    } catch (error: any) {
+      message.error(error.message);
+    } finally {
+      setState((prev) => ({
+        ...prev,
+        isLoading: false,
+      }));
+    }
   };
 
   return (
@@ -92,6 +161,24 @@ const AddBookPage = () => {
               />
             </Form.Item>
             <Form.Item
+              name={"pageCount"}
+              label={"Số trang"}
+              required
+              rules={[
+                {
+                  type: "number",
+                  message: "Số trang sách phải là một số",
+                },
+              ]}
+            >
+              <InputNumber
+                style={{ width: "100%" }}
+                suffix="Trang"
+                inputMode="decimal"
+                alt="Book Total Page Input"
+              />
+            </Form.Item>
+            <Form.Item
               name={"description"}
               label={"Mô tả"}
               required
@@ -104,30 +191,49 @@ const AddBookPage = () => {
             >
               <Input.TextArea allowClear />
             </Form.Item>
-            <Form.Item label="File sách">
-              <Dragger
-                name="pdfFile"
-                multiple={false}
-                accept=".pdf"
-                customRequest={(info) => {
-                  setFileUpload(info.file);
-                }}
-                className="m-3"
+            <Form.Item label={"Nhà xuất bản"}>
+              <Select
+                showSearch
+                optionFilterProp="label"
+                labelInValue={true}
+                optionLabelProp="value"
+                onChange={onChangePublisherSelect}
+                onSearch={onSearchPublisherSelect}
+              >
+                {state.publishers.map((index) => (
+                  <Select.Option key={index.id} value={index.name}>
+                    {index.name}
+                  </Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+
+            <Form.Item label="File" name="file">
+              <input
                 style={{
                   width: "100%",
-                  maxHeight: "300px",
                 }}
-              >
-                <p className="ant-upload-drag-icon">
-                  <InboxOutlined />
-                </p>
-                <p className="ant-upload-text">
-                  Click hoặc thả file vào vùng này để tải lên
-                </p>
-              </Dragger>
+                id="file-upload"
+                name="file"
+                type="file"
+                accept=".pdf"
+                aria-label="Upload a PDF file"
+                onChange={(event) => {
+                  if (event.target.files) {
+                    setFileUpload(event.target.files[0]);
+                  }
+                }}
+              />
             </Form.Item>
-            <Button onClick={() => addBookForm.submit()} type="primary">
-              Thêm
+
+            <Button
+              type="primary"
+              onClick={handleAddBook}
+              disabled={fileUpload === undefined}
+              loading={state.isLoading}
+              style={{ marginTop: 16 }}
+            >
+              {state.isLoading ? "Uploading" : "Start Upload"}
             </Button>
           </Form>
         </Card>
