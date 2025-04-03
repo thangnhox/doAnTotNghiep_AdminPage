9216781eaccdd266dfdbe3f55c51c45de8cfa511
Ton Duc Thang <tncthng347@gmail.com>
2025-04-03 15:49:32 +0700

    Implement CategoryPage

--- thu-vien-sach/src/models/Category.ts
+++ thu-vien-sach/src/models/Category.ts
@@ -1,4 +1,12 @@
 export default interface Category {
   id: number;
   name: string;
+  booklist?: {
+    cover_url: string;
+    BookID: number;
+    Price: number;
+    Title: string;
+    PageCount: number;
+  }[];
+  books?: number;
 }
--- thu-vien-sach/src/pages/main/category/CategoryDetail.tsx
+++ thu-vien-sach/src/pages/main/category/CategoryDetail.tsx
@@ -1,10 +1,106 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import Category from "../../../models/Category";
+import { useEffect, useState } from "react";
+import { AxiosResponse } from "axios";
+import { ResponseDTO } from "../../../dtos/Response/ResponseDTO";
+import { handleAPI } from "../../../remotes/apiHandle";
+import { Button, Card, Divider, Empty, Image, Spin, Typography } from "antd";
 
 const CategoryDetail = () => {
   const { id } = useParams();
+  const [category, setCategory] = useState<Category | null>(null);
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const navigate = useNavigate();
 
-  console.log(id);
-  return <div>CategoryDetail</div>;
+  const getCategory = async () => {
+    try {
+      setLoading(true);
+      const res: AxiosResponse<ResponseDTO<Category>> = await handleAPI(
+        `categories/fetch/${id}?detail=true`
+      );
+
+      setCategory(res.data.data);
+    } catch (error: any) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getCategory();
+  }, []);
+
+  if (isLoading) {
+    return <Spin />;
+  }
+
+  if (!category || category.booklist?.length === 0) {
+    console.log(category);
+    return (
+      <div className="d-flex flex-column gap-3 mt-3">
+        <Empty />
+      </div>
+    );
+  }
+
+  return (
+    <div className="d-flex flex-column gap-3 mt-3">
+      <Typography.Title level={3}>{category?.name}</Typography.Title>
+      <Divider />
+      <div className="container">
+        <div className="row">
+          {category?.booklist?.map((book) => (
+            <div className="col" key={book.BookID}>
+              <Card
+                title={
+                  <Typography.Title level={4}>
+                    {book.Title}
+                  </Typography.Title>
+                }
+                style={{
+                  width: 400,
+                  borderRadius: 10,
+                  overflow: "hidden",
+                  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                }}
+              >
+                <div className="d-flex flex-column align-items-center">
+                  <Image
+                    src={book.cover_url}
+                    preview={false}
+                    width={150}
+                    height={200}
+                    style={{ borderRadius: 8 }}
+                  />
+
+                  <div className="mt-3 text-center">
+                    <Typography.Text className="d-block mb-1">
+                      <strong>Giá: </strong> {book.Price} VND
+                    </Typography.Text>
+                    <Typography.Text>
+                      <strong>Số trang: </strong> {book.PageCount}
+                    </Typography.Text>
+                  </div>
+
+                  <div className="d-flex flex-row gap-3 align-items-center justify-content-center mt-3">
+                    <Button
+                      type="primary"
+                      className="bg-success"
+                      onClick={() => navigate(`/books/detail/${book.BookID}`)}
+                      style={{ borderRadius: 5 }}
+                    >
+                      Chi tiết
+                    </Button>
+                  </div>
+                </div>
+              </Card>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CategoryDetail;
